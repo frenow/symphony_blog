03ca3198d3bc6d5bbf67e27cc30b15202f972f88
Emerson Duarte De Amorim <560766@sga.pucminas.br>
2019-12-19 21:23:30 -0300

    add functional tests

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -3,25 +3,40 @@ namespace App\Controller;
 
 use App\Entity\Post;
 use Doctrine\ORM\EntityManagerInterface;
+use App\Exception\ValidationException;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Serializer\SerializerInterface;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+
 
 final class PostController {
     private EntityManagerInterface $entityManager;
+    private SerializerInterface $serializer;
+    private ValidatorInterface $validator;
 
-    public function __construct(EntityManagerInterface $entityManager) {
+    public function __construct(EntityManagerInterface $entityManager, SerializerInterface $serializer, ValidatorInterface $validator) {
         $this->entityManager = $entityManager;
+        $this->serializer = $serializer;
+        $this->validator = $validator;
     }
 
 /**
  * @Route("/posts", methods={"POST"})
  */
 public function create(Request $request): Response {
-    $data = json_decode($request->getContent(), true);
+//    $data = json_decode($request->getContent(), true);
+//    $post = new Post($data['title'], $data['description']);
+    $post = $this->serializer->deserialize($request->getContent(), Post::class, 'json');
 
-    $post = new Post($data['title'], $data['description']);
+    $errors = $this->validator->validate($post);
+    if (count($errors)){
+        throw new ValidationException($errors);
+    }
+
     $this->entityManager->persist($post);
     $this->entityManager->flush();
 
@@ -33,14 +48,19 @@ public function create(Request $request): Response {
  */
 public function details(int $id): Response {
 /** @var Post $post */
-$post = $this->entityManager->getRepository(Post::class)->find($id);
+    $post = $this->entityManager->getRepository(Post::class)->find($id);
 
-    return JsonResponse::create([
-    'id' => $post->getId(),
-    'title' => $post->title,
-    'description' => $post->description,
-    'createdAt' => $post->getCreatedAt()->format('Y-m-d'),
-    ]);
+    if (null === $post){
+        throw new NotFoundHttpException('Post não encontrado');
+    }
+
+//    return JsonResponse::create([
+//    'id' => $post->getId(),
+//    'title' => $post->title,
+//    'description' => $post->description,
+//    'createdAt' => $post->getCreatedAt()->format('Y-m-d'),
+//    ]);
+    return JsonResponse::fromJsonString($this->serializer->serialize($post, 'json'));
   }
 
 /**
@@ -49,17 +69,19 @@ $post = $this->entityManager->getRepository(Post::class)->find($id);
 public function index(): Response {
     /** @var Post [$posts] */
     $posts = $this->entityManager->getRepository(Post::class)->findAll();
-    $data = [];
+    //$data = [];
 
-    foreach($posts as $post) {
-        $data[] =  [
-        'id' => $post->getId(),
-        'title' => $post->title,
-        'description' => $post->description,
-        'createdAt' => $post->getCreatedAt()->format('Y-m-d'),
-        ];
-    }
-    return JsonResponse::create($data);
+    //foreach($posts as $post) {
+    //    $data[] =  [
+    //    'id' => $post->getId(),
+    //    'title' => $post->title,
+    //    'description' => $post->description,
+    //    'createdAt' => $post->getCreatedAt()->format('Y-m-d'),
+    //   ];
+    //}
+
+    //return JsonResponse::create($data);
+    return JsonResponse::fromJsonString($this->serializer->serialize($posts, 'json'));    
   }  
 
 /**
--- tests/Controller/PostControllerTest.php
+++ tests/Controller/PostControllerTest.php
@@ -1,24 +1,60 @@
 <?php
 
 namespace App\tests\Controller;
+use App\Entity\Post;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\KernelBrowser;
+use Doctrine\ORM\Tools\SchemaTool;
 
 Class PostControllerTest extends WebTestCase {
+private EntityManagerInterface $em;
+private KernelBrowser $client;
+
+public function setUp(): void {
+    $this->client = self::createClient();
+    $this->em = self::$kernel->getContainer()->get('doctrine')->getManager();   
+    //CRIA A FERRAMENTA PARA MANIPULAÇÃO DO BANCO DE DADOS
+    $tool = new SchemaTool($this->em);
+
+    //recupera a tabela associada a entidade post
+    $metadata = $this->em->getClassMetadata(Post::class);
+
+    //apaga a tabela associada a entidade post
+    $tool->dropSchema([$metadata]);
+
+    try {
+        //cria a tabela post
+        $tool->createSchema([$metadata]);
+    } catch (ToolsExceptions $e) {
+        $this->fail("Impossivel criar a tabela post". $e->getMessage());
+    }
+}    
 
 public function test_create_post(): void {
-    $client = static::createClient();
-    $client->request('POST','/posts', [], [], [], json_encode([
+    $this->client->request('POST','/posts', [], [], [], json_encode([
     'title' => 'Minha primeira aplicação com symfony',
     'description' => 'Descrição da minha primeira aplicação'
     ]));
-    $this->assertEquals(Response::HTTP_CREATED, $client->getResponse()->getStatusCode());
+    $this->assertEquals(Response::HTTP_CREATED, $this->client->getResponse()->getStatusCode());
 }
 
 public function test_delete_post(): void {
-    $client = static::createClient();
-    $client->request('DELETE','/posts/3');
-    $this->assertEquals(Response::HTTP_NO_CONTENT, $client->getResponse()->getStatusCode());
+    $post = new Post("fake", "fake");
+    $this->em->persist($post);
+    $this->em->flush();
+    
+    $this->client->request('DELETE','/posts/1');
+    $this->assertEquals(Response::HTTP_NO_CONTENT, $this->client->getResponse()->getStatusCode());
+}
+
+public function test_create_post_with_invalid_title():void{
+    $this->client->request('POST', '/posts',[],[],[], json_encode([
+        'title'=>1234,
+        'description'=>'alguma descrição'
+    ]));
+    $this->assertEquals(Response::HTTP_BAD_REQUEST, $this->client->getResponse()->getStatusCode());
 }
 
 }
